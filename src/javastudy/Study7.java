6ae777e3c4e77db7ae97be03ecec42522428de62
inasa <inasaki0704@outlook.jp>
2021-02-22 12:41:13 +0900

    課題

---
+++ src/javastudy/Study7.java
@@ -0,0 +1,21 @@
+package javastudy;
+
+public class Study7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1
+		for(int a = 1 ;a <= 9 ; a++) {
+			System.out.println(7 + " ~ " + a + " = " + a * 7);
+		}
+		
+		//2
+		int b = 8 ;
+		int answer = 1 ; 
+		for(int c = 1 ; c <= b ; c++) {
+			answer = answer * 2;
+			System.out.println("2‚Ì" + c + "æ‚Í" + answer);
+		}
+	}
+
+}
---
+++ src/javastudy/Study8.java
@@ -0,0 +1,15 @@
+package javastudy;
+
+public class Study8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1 ; i <= 9 ; i++) {
+			for(int n = 1 ; n<= 9 ; n++) {
+				System.out.print(i * n + " ");
+			}
+				System.out.println();
+			}
+	}
+
+}
